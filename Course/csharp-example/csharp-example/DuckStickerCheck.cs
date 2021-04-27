2745ab72975b800da152dffa9d626a6217275378
JuliaKozhemyachenko <82221149+JuliaKozhemyachenko@users.noreply.github.com>
2021-04-27 19:14:06 +0300

    Задание 8. Сделайте сценарий, проверяющий наличие стикеров у товаров

---
+++ Course/csharp-example/csharp-example/DuckStickerCheck.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class DuckStickerCheck
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://localhost/litecart/en/";
+            wait.Until(ExpectedConditions.UrlContains("http://localhost/litecart/en/"));
+            var products = driver.FindElements(By.CssSelector("li.product")).Count;
+            var lasttstickers = driver.FindElements(By.CssSelector("li.product div.sticker:last-child")).Count;
+            Assert.IsTrue(products == lasttstickers);
+
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
