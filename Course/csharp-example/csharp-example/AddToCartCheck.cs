93572cedf4eb0b878a47f2b9220da589c76be540
JuliaKozhemyachenko <82221149+JuliaKozhemyachenko@users.noreply.github.com>
2021-04-29 22:10:59 +0300

    Задание 13. Сделайте сценарий работы с корзиной

---
+++ Course/csharp-example/csharp-example/AddToCartCheck.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class AddToCartCheck
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new FirefoxDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://localhost/litecart/en/";
+            wait.Until(ExpectedConditions.UrlContains("http://localhost/litecart/en/"));
+
+            for (int count = 0; count < 3; count++)
+            {
+                wait.Until(ExpectedConditions.ElementExists(By.CssSelector("div.content a.link")));
+                driver.FindElement(By.CssSelector("div.content a.link")).Click();
+
+                wait.Until(ExpectedConditions.ElementExists(By.Name("add_cart_product")));
+                if (driver.FindElements(By.CssSelector("div.buy_now select")).Count != 0)
+                {
+                    driver.FindElement(By.CssSelector("div.buy_now select")).Click();
+                    driver.FindElement(By.CssSelector("div.buy_now select option[value=Small]")).Click();
+                }
+                driver.FindElement(By.Name("add_cart_product")).Click();
+
+                var now = DateTime.Now;
+                wait.Until(wd => (DateTime.Now - now).TotalSeconds > 1);
+                driver.FindElement(By.CssSelector("#site-menu li.general-0 a")).Click();
+            }
+            driver.FindElement(By.CssSelector("#cart a.link")).Click();
+            wait.Until(ExpectedConditions.ElementExists(By.CssSelector("#order_confirmation-wrapper")));
+
+            while (driver.FindElements(By.Name("remove_cart_item")).Count > 0)
+            {
+                var rowsCount = driver.FindElements(By.CssSelector("#order_confirmation-wrapper tr")).Count;
+                driver.FindElement(By.Name("remove_cart_item")).Click();
+                wait.Until(wd => wd.FindElements(By.CssSelector("#order_confirmation-wrapper tr")).Count != rowsCount);
+            }
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
